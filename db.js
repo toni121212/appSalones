3098da6a914e0eb108af3e9eb32e73ce74a77299
toni121212 <rivasgonzalezluisantonio@gmail.com>
2026-02-27 18:43:34 -0600

    Arreglar mysql

--- db.js
+++ db.js
@@ -1,5 +1,13 @@
 const mysql = require("mysql2/promise");
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
+const pool = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT,
+  waitForConnections: true,
+  connectionLimit: 10
+});
 
 module.exports = pool;
